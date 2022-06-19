b094a4555d3b269572edf2cffda8f9e185c59951
Andy Tom <attom2@illinois.edu>
2022-06-19 17:14:52 -0500

    add try catch around search api call

--- src/lib/SymbolSearch.svelte
+++ src/lib/SymbolSearch.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
 	import LinearProgress from '@smui/linear-progress';
 	import Textfield from '@smui/textfield';
-  import HelperText from '@smui/textfield/helper-text';
-	import List, {
-			Item,
-			Meta,
-			Text,
-			PrimaryText,
-			SecondaryText,
-		} from '@smui/list';
+	import HelperText from '@smui/textfield/helper-text';
+	import List, { Item, Meta, Text, PrimaryText, SecondaryText } from '@smui/list';
 
 	let previewSearch: Symbol[] = [];
 	let search: string = '';
@@ -19,53 +13,45 @@
 	};
 
 	const searchForSymbols = async () => {
-		if(!search.length) {
+		if (!search.length) {
+			previewSearch = [];
 			return;
-		} 
-		const response = await fetch(`https://vast.tastyworks.com/symbols/search/${search}`);
-		const formattedResp = await response.json();
-		previewSearch = formattedResp?.data?.items;
+		}
+		try {
+			const response = await fetch(`https://vast.tastyworks.com/symbols/search/${search}`);
+			const formattedResp = await response.json();
+			previewSearch = formattedResp?.data?.items;
+		} catch {
+			previewSearch = [];
+		}
 	};
 
 	const symbolClick = (item: Symbol) => {
-		selection = item.symbol
-		previewSearch = []
-	}
- 
-  let value: string | undefined = undefined;
- 
-  let selection = '';
-	
+		selection = item.symbol;
+		previewSearch = [];
+	};
+
+	let selection = '';
 </script>
 
-<div class="counter">
-</div>
+<div class="counter" />
 <div class="search">
 	<Textfield class="full-width" label="Symbol" bind:value={search} on:input={searchForSymbols}>
-      <HelperText persistent slot="helper">EX: AAPL, GOOG</HelperText>
-  </Textfield>
-	<List
-		class="list mt-1"
-    twoLine
-    singleSelection
-		dense
-  >
-    {#each previewSearch as item}
-      <Item
-        on:click={() => symbolClick(item)}
-        selected={selection === item.symbol}
-      >
-        <Text>
-          <PrimaryText>{item.symbol}</PrimaryText>
-          <SecondaryText>{item.description}</SecondaryText>
-        </Text>
-				{
-					#if item.options}
-        <Meta class="material-icons">info</Meta>
+		<HelperText persistent slot="helper">EX: AAPL, GOOG</HelperText>
+	</Textfield>
+	<List class="list mt-1" twoLine singleSelection dense>
+		{#each previewSearch as item}
+			<Item on:click={() => symbolClick(item)} selected={selection === item.symbol}>
+				<Text>
+					<PrimaryText>{item.symbol}</PrimaryText>
+					<SecondaryText>{item.description}</SecondaryText>
+				</Text>
+				{#if item.options}
+					<Meta>Options</Meta>
 				{/if}
-      </Item>
-    {/each}
-  </List>
+			</Item>
+		{/each}
+	</List>
 </div>
 
 <style>
@@ -81,12 +67,11 @@
 	}
 
 	* :global(.list) {
-    max-height: 400px;
+		max-height: 400px;
 		overflow-y: scroll;
-  }
+	}
 
 	* :global(.full-width) {
 		width: 100%;
 	}
-
 </style>
